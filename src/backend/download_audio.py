c06fc03b81c06ed97275fb9201e99761b6aea213
abdo-28 <Gamal_Ketchup@proton.me>
2025-09-28 20:53:28 +0300

    Streamlined download code
    still very basic but it gets the job done somewhat

--- src/backend/download_audio.py
+++ src/backend/download_audio.py
@@ -1,12 +1,22 @@
-from yt_dlp import YoutubeDL
-from .video_object import MediaObject
+import os
+from pathlib import Path
 
-def download_audio(media_object: MediaObject):
-    try:
-        url = media_object.url
-        opts = {'quiet': True, 'format': 'm4a/bestaudio/best',  'postprocessors': [
-            { 'key': 'FFmpegExtractAudio', 'preferredcodec':'m4a', },
-            ], }
-            YoutubeDL(opts).download([url])
-    except:
-        return
+from yt_dlp import YoutubeDL
+
+def download_audio(url: str, path: Path or str):
+    path = Path(path)
+    parent_path = path.parent
+
+    # ensuring requested path exists
+    if not parent_path.is_dir():
+        os.makedirs(parent_path) # create it if it doesn't
+
+    opts = {'quiet': True, 'format': 'm4a/bestaudio/best', 'outtmpl': str(path), 'postprocessors': [
+        { 'key': 'FFmpegExtractAudio', 'preferredcodec':'flac', },
+        ], }
+    YoutubeDL(opts).download([url])
+
+    file_format = opts['postprocessors'][0]['preferredcodec']
+    new_path = Path(f'{path}.{file_format}')
+
+    return new_path 
--- src/backend/download_audio_with_metadata.py
+++
@@ -1,7 +0,0 @@
-from .embed_metadata import embed_metadata
-from .download_audio import download_audio
-from .video_object import MediaObject
-
-def download_audio_with_metadata(media_object: MediaObject):
-    download_audio(media_object)
-    embed_metadata()
