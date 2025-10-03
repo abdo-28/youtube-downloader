0841892e7b99d12a14f11f8c0f59698c551a8d93
abdo-28 <Gamal_Ketchup@proton.me>
2025-10-03 22:57:57 +0300

    just some misc edits
    just adding some stuff to the __init__ of the backend package and
    changing main.py to incorporate the parse_url function.

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -1,4 +1,7 @@
+opts = {'quiet': True} # universal options for YoutubeDL
+
 from .metadata import Metadata
 from .video_object import MediaObject
+from .sort_urls import parse_url 
 from .download_audio import download_audio
 from .embed_metadata import embed_metadata
--- src/main.py
+++ src/main.py
@@ -2,26 +2,30 @@ from datetime import date
 from pathlib import Path
 import json
 
-from backend import MediaObject, download_audio, embed_metadata
+from backend import MediaObject, download_audio, embed_metadata, parse_url 
+
+entries = list()
 
 with open('download.json') as f:
     urls = json.load(f)
 
-for i in urls:
+for url in urls:
+    result = parse_url(url)
+    if result is list or result is tuple:
+        entries.extend(result)
+    else:
+        entries.append(result)
+
+for i in entries:
     print(f'Downloading -> {i}')
+
     media_object = MediaObject(i)
 
     metadata = media_object.selected_metadata_object
-    if type(metadata.artist) is list:
-        artist = metadata.artist[0]
-    elif type(metadata.artist) is str:
-        artist = metadata.artist
-
-    media_object.media_file = Path('Music', artist, metadata.album, metadata.title)
+    media_object.media_file = Path('Music', metadata.artist, metadata.album, metadata.title)
 
     url = media_object.source
     path = media_object.media_file
     new_path = download_audio(url,path)
     embed_metadata(new_path,metadata)
-
     print('Done')
